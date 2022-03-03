9e3b849448b688e68c4b73f9211e9556594f47f2
surajaiswal13@gmail.com <surajaiswal13@gmail.com>
2022-03-04 02:20:52 +0530

    [UPDATE] Map function Implemented

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,28 +2,26 @@ import React from "react";
 import Card from "./Card"
 import contacts from "../contacts.js"
 
+function createCard(contact) {
+  return (
+    <Card 
+      heading={contact.name} 
+      img={contact.imgURL}
+      mail={contact.email}
+      number={contact.phone}
+      />
+  )
+}
+
 function App(props) {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Card 
-      heading={contacts[0].name} 
-      img={contacts[0].imgURL}
-      mail={contacts[0].email}
-      number={contacts[0].phone}
-      />
-    <Card 
-      heading={contacts[1].name} 
-      img={contacts[1].imgURL}
-      mail={contacts[1].email}
-      number={contacts[1].phone}
-      />
-      <Card 
-      heading={contacts[2].name} 
-      img={contacts[2].imgURL}
-      mail={contacts[2].email}
-      number={contacts[2].phone}
-      />
+
+      {/* Map function wull loop through every contact in array and call createCard function */}
+
+      {contacts.map(createCard)}
+      
     </div>
   );
 }
